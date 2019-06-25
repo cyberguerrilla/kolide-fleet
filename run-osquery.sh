21dcacfbabdf97d922e00561e4991c595160b53b
analyst <analyst@elk.windomain.local>
2019-06-25 00:30:55 -0500

    Update run-osquery.sh to remove sleep

--- run-osquery.sh
+++ run-osquery.sh
@@ -4,8 +4,6 @@ ansible-playbook /opt/playbooks/kolide-fleet/update-run-fleet-containers.yml
 
 echo "Waiting for osquery to start"
 
-sleep 60
-
 cd /opt/playbooks/windows-dc
 
 /opt/playbooks/windows-dc/config-osquery.sh
